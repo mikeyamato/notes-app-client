4d9e55c08575a5fd3e2cc356886a7b872cde6ece
Mike Yamato <mike.yamato@mobileprogramming.com>
2019-04-23 10:24:10 -0700

    updated files

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -34,7 +34,7 @@ export default class Login extends Component {
     try {
       await Auth.signIn(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
-      this.props.history.push("/");
+      // this.props.history.push("/");   // removed to allow for referenced pages to load after login
     } catch (e) {
       alert(e.message);
       this.setState({ isLoading: false });
